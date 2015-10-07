6a0fe6bf1dd060d03fbbd4f62c53224a3d7ba0c6
Ruiqi Zhang <rzhang@RZHANG-W2.roundarch.com>
2015-10-07 16:15:39 -0400

    MidiPlayer

---
+++ MidiPlayer.py
@@ -0,0 +1,37 @@
+import pygame
+import pygame.midi
+import time
+from enum import Enum
+
+class MIDIPlayer:
+	
+	class Notes(Enum):
+		sixteenth=.0625
+		eighth=.125
+		quarter=.25
+		half=.5
+		whole=1
+
+
+	def __init__(self, port, instrument):
+		pygame.midi.init()
+		
+		print "using port %s" %port
+		self.player=pygame.midi.Output(port,0)
+		#piano
+		print "using instrument %s" %instrument
+		self.player.set_instrument(instrument,port)
+
+	def go(self, note, duration):
+		self.player.note_on(note,127)
+		time.sleep(duration)
+		self.player.note_off(note,127)
+
+if __name__ == '__main__':
+	musicPlyr = MIDIPlayer(0, 0)
+	notes=MIDIPlayer.Notes
+	fur=[(76,notes.quarter),(75,notes.quarter),(76,notes.quarter),(75,notes.quarter),(76,notes.quarter),(71,notes.quarter),(74,notes.quarter),(72,notes.quarter),(69,notes.whole)]
+	for x in fur:
+		musicPlyr.go(x[0],x[1].value)
+	del musicPlyr
+	pygame.midi.quit()

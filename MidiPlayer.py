383f1431b8e0e1882437ee761470703cc7c680f5
Ruiqi Zhang <rzhang@RZHANG-W2.roundarch.com>
2015-10-08 11:21:45 -0400

    expanded functionality: added loundess and tempo

--- MidiPlayer.py
+++ MidiPlayer.py
@@ -6,29 +6,35 @@ from enum import Enum
 class MIDIPlayer:
 	
 	class Notes(Enum):
-		sixteenth=.0625
-		eighth=.125
-		quarter=.25
-		half=.5
+		sixtyforth=0.015625
+		thirtysecondth=0.03125
+		sixteenth=0.0625
+		eighth=0.125
+		quarter=0.25
+		half=0.5
 		whole=1
 
 
-	def __init__(self, port, instrument):
+	def __init__(self, port, instrument, tempo=60):
 		pygame.midi.init()
 		
 		print "using port %s" %port
 		self.player=pygame.midi.Output(port,0)
 		#piano
+		self.player.tempo=60.0/tempo
 		print "using instrument %s" %instrument
 		self.player.set_instrument(instrument,port)
-
-	def playNote(self, note, duration):
-		self.player.note_on(note,127)
-		time.sleep(duration)
-		self.player.note_off(note,127)
+	# note less than means rest
+	def playNote(self, note, duration, loudness=127):
+		if note >= 21:
+			self.player.note_on(note,loudness)
+			time.sleep(self.player.tempo*duration)
+			self.player.note_off(note,loudness)
+		else:
+			time.sleep(duration)
 
 if __name__ == '__main__':
-	musicPlyr = MIDIPlayer(0, 0)
+	musicPlyr = MIDIPlayer(0, 0,90)
 	notes=MIDIPlayer.Notes
 	fur=[(76,notes.quarter),(75,notes.quarter),(76,notes.quarter),(75,notes.quarter),(76,notes.quarter),(71,notes.quarter),(74,notes.quarter),(72,notes.quarter),(69,notes.whole)]
 	for x in fur:
